a4c3b0185b37c73ef444c9ed082808b8febc89c5
rismanitalst <rismanitalst@gmail.com>
2025-12-25 21:34:41 +0700

    stl lottie

---
+++ lib/splash/splash_lottie.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyLottie extends StatefulWidget {
+  const MyLottie({super.key});
+
+  @override
+  State<MyLottie> createState() => _MyLottieState();
+}
+
+class _MyLottieState extends State<MyLottie> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
