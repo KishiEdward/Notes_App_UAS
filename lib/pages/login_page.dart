6cadc1484819152042dcac27c147f3a79ae1d51a
rismanitalst <rismanitalst@gmail.com>
2025-12-26 15:43:39 +0700

    menghapus import HomePage yang tidak terpakai

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:notesapp/pages/register_page.dart';
 import 'package:notesapp/services/auth_service.dart';
-import 'package:notesapp/pages/home_page.dart';
+
 import 'package:notesapp/pages/forgot_password-e.dart';
 import 'package:notesapp/splash/splash_lottie.dart';
 
