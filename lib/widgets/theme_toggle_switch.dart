5abf887ff9a8fcbe533e7536bfa8e04737b4d2c2
DavidDSaputra <davidsaputra2704@gmail.com>
2025-12-29 20:52:35 +0700

    ganti toggle jadi kucing imut

--- lib/widgets/theme_toggle_switch.dart
+++ lib/widgets/theme_toggle_switch.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:lottie/lottie.dart';
 
-class ThemeToggleSwitch extends StatefulWidget {
+class ThemeToggleSwitch extends StatelessWidget {
   final bool value;
   final ValueChanged<bool> onChanged;
   final double width;
@@ -16,72 +16,18 @@ class ThemeToggleSwitch extends StatefulWidget {
   });
 
   @override
-  State<ThemeToggleSwitch> createState() => _ThemeToggleSwitchState();
-}
-
-class _ThemeToggleSwitchState extends State<ThemeToggleSwitch> with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 500), // Durasi transisi
-    );
-
-    // Set kondisi awal tanpa animasi
-    // Coba dibalik: 0.0 = Malam, 1.0 = Siang (berdasarkan feedback user)
-    // Atau mungkin file lottie nya yang memang start from Night?
-    // Mari coba balik value nya.
-    if (widget.value) {
-      _controller.value = 0.0; // Dark mode (Malam) - Asumsi frame 0
-    } else {
-      _controller.value = 0.5; // Light mode (Siang) - Asumsi animasi di tengah atau akhir
-      // Tapi tunggu, biasanya toggle linear.
-      // Coba set full reverse:
-      // Dark (True) -> 1.0
-      // Light (False) -> 0.0
-      
-      // Jika user bilang Light stop di Malam, berarti 0.0 = Malam.
-      // Maka Light harusnya 1.0 (Siang)?
-      _controller.value = 1.0;
-    }
-  }
-
-  @override
-  void didUpdateWidget(ThemeToggleSwitch oldWidget) {
-    super.didUpdateWidget(oldWidget);
-    if (oldWidget.value != widget.value) {
-      if (widget.value) {
-        // Light -> Dark (Siang -> Malam)
-        // Jika 1.0 = Siang, 0.0 = Malam
-        _controller.reverse(); 
-      } else {
-        // Dark -> Light (Malam -> Siang)
-        _controller.forward();
-      }
-    }
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  @override
   Widget build(BuildContext context) {
     return GestureDetector(
       onTap: () {
-        widget.onChanged(!widget.value);
+        onChanged(!value);
       },
       child: SizedBox(
-        width: widget.width,
-        height: widget.height,
+        width: width,
+        height: height,
         child: Lottie.asset(
-          'assets/animations/Day_and_Night_Toggle.json',
-          controller: _controller,
+          value
+              ? 'assets/animations/Black Cat Green Eyes Peeping.json'
+              : 'assets/animations/White Cat Peeping.json',
           fit: BoxFit.cover,
         ),
       ),
