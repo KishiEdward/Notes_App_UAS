1a7196315e8ea9616b84270d8b12880ea04357be
DavidDSaputra <davidsaputra2704@gmail.com>
2025-12-29 18:47:25 +0700

    improve auth wrapper dengan silent check

--- lib/widgets/auth_wrapper.dart
+++ lib/widgets/auth_wrapper.dart
@@ -2,12 +2,54 @@ import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:notesapp/splash/splash1.dart';
 import 'package:notesapp/pages/home_page.dart';
+import 'package:notesapp/services/auth_service.dart';
 
-class AuthWrapper extends StatelessWidget {
+class AuthWrapper extends StatefulWidget {
   const AuthWrapper({super.key});
 
   @override
+  State<AuthWrapper> createState() => _AuthWrapperState();
+}
+
+class _AuthWrapperState extends State<AuthWrapper> {
+  bool _isChecking = true;
+
+  @override
+  void initState() {
+    super.initState();
+    _checkAuth();
+  }
+
+  Future<void> _checkAuth() async {
+    await Future.delayed(const Duration(milliseconds: 500));
+    
+    final user = FirebaseAuth.instance.currentUser;
+    if (user == null) {
+      final authService = AuthService();
+      final isGoogleSignedIn = await authService.isGoogleSignedIn();
+      
+      if (isGoogleSignedIn) {
+        await authService.silentGoogleSignIn();
+      }
+    }
+    
+    if (mounted) {
+      setState(() {
+        _isChecking = false;
+      });
+    }
+  }
+
+  @override
   Widget build(BuildContext context) {
+    if (_isChecking) {
+      return const Scaffold(
+        body: Center(
+          child: CircularProgressIndicator(),
+        ),
+      );
+    }
+
     return StreamBuilder<User?>(
       stream: FirebaseAuth.instance.authStateChanges(),
       builder: (context, snapshot) {
