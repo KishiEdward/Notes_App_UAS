4fd16f0303fe80431cbf3f11cdb742fe1472787b
DavidDSaputra <davidsaputra2704@gmail.com>
2025-12-30 16:30:18 +0700

    import google fonts ke splash2

--- lib/splash/splash2.dart
+++ lib/splash/splash2.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:notesapp/splash/splash3.dart';
 import 'package:notesapp/pages/login_page.dart';
 
