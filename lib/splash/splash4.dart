827c51f79ff5e4d71c8ce8c34627d17f4af35517
rismanitalst <rismanitalst@gmail.com>
2026-01-01 16:58:06 +0700

    tambah padding layout splash4

--- lib/splash/splash4.dart
+++ lib/splash/splash4.dart
@@ -5,17 +5,27 @@ class Splash4 extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       body: SafeArea(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children:[
-            Text('Gambar Bulat'),
-            SizedBox(height: 24),
-            Text('Judul'),
-            SizedBox(height: 12),
-            Text('Subjudul'),
-          ],
+        child: Center(
+          child: Padding(
+            padding: EdgeInsets.symmetric(horizontal: 24),
+            child: ConstrainedBox(
+              constraints: BoxConstraints(
+                maxWidth: 420,
+              ),
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Text('Gambar Bulat'),
+                  SizedBox(height: 24),
+                  Text('Judul'),
+                  SizedBox(height: 12),
+                  Text('Subjudul'),
+                ],
+              ),
+            ),
+          ),
         ),
       ),
     );
