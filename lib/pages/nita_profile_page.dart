f278c2879de34b8ad8977c8ad00eadb9e13a6172
rismanitalst <rismanitalst@gmail.com>
2026-01-07 00:16:23 +0700

    tambah background dan overlay halaman profile

--- lib/pages/nita_profile_page.dart
+++ lib/pages/nita_profile_page.dart
@@ -4,7 +4,7 @@ import 'package:google_fonts/google_fonts.dart';
 class NitaProfilePage extends StatelessWidget {
   const NitaProfilePage({super.key});
 
-  //pallete warna yg digunakan
+  // pallete warna yg digunakan
   static const Color roseQuartz = Color(0xFFF9CBD6);
   static const Color blush = Color(0xFFF2AFBC);
   static const Color redWine = Color(0xFF9E182B);
@@ -13,16 +13,31 @@ class NitaProfilePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: oatMilk,
-      body: Center(
-        child: Text(
-          'Profile Nita',
-          style: GoogleFonts.poppins(
-            fontSize: 18,
-            fontWeight: FontWeight.w600,
-            color: redWine,
+      body: Stack(
+        children: [
+          Positioned.fill(
+            child: Image.asset(
+              'assets/images/cherry_bg.jpg',
+              fit: BoxFit.cover,
+            ),
           ),
-        ),
+          Positioned.fill(
+            child: Container(
+              color: Colors.white.withOpacity(0.65),
+            ),
+          ),
+          SafeArea(
+            child: Center(
+              child: Text(
+                'profile nita',
+                style: GoogleFonts.poppins(
+                  fontSize: 18,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
