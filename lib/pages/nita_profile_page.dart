7520c850179cf63453506a48ed71ae461fc83e7f
rismanitalst <rismanitalst@gmail.com>
2026-01-06 23:23:58 +0700

    tambah palette warna dasar pink

--- lib/pages/nita_profile_page.dart
+++ lib/pages/nita_profile_page.dart
@@ -3,6 +3,12 @@ import 'package:flutter/material.dart';
 class NitaProfilePage extends StatelessWidget {
   const NitaProfilePage({super.key});
 
+  //pallete warna yg digunakan
+  static const Color roseQuartz = Color(0xFFF9CBD6);
+  static const Color blush = Color(0xFFF2AFBC);
+  static const Color redWine = Color(0xFF9E182B);
+  static const Color oatMilk = Color(0xFFF2E0D2);
+
   @override
   Widget build(BuildContext context) {
     return const Scaffold(
