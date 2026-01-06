0ef0dcadcc092e4bfe5124d9d234f172df3de95a
rismanitalst <rismanitalst@gmail.com>
2026-01-06 23:15:09 +0700

    buat halaman profile nita

---
+++ lib/pages/nita_profile_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class NitaProfilePage extends StatelessWidget {
+  const NitaProfilePage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: Center(
+        child: Text('profile nita'),
+      ),
+    );
+  }
+}
